a3cf9fa2dc0aa1c4bdda2cd1e8463091844894fb
Doan Quoc Thong <thongdoan742@gmail.com>
2025-11-16 11:20:58 +0700

    Thong commit - Get order & Change order status

--- src/main/java/com/java/TMDTPicnic/dto/request/OrderStatusUpdateRequest.java
+++ src/main/java/com/java/TMDTPicnic/dto/request/OrderStatusUpdateRequest.java
@@ -1,10 +1,14 @@
 package com.java.TMDTPicnic.dto.request;
 
 import com.java.TMDTPicnic.enums.OrderStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class OrderStatusUpdateRequest {
     private OrderStatus status;
--- src/main/java/com/java/TMDTPicnic/dto/response/OrderPageResponse.java
+++ src/main/java/com/java/TMDTPicnic/dto/response/OrderPageResponse.java
@@ -2,18 +2,18 @@ package com.java.TMDTPicnic.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.apache.poi.ss.formula.functions.T;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
-public class ProductPageResponse {
-    private List<ProductResponse> content;
-    private int pageNumber;
-    private int pageSize;
+public class OrderPageResponse {
+    private List<OrderSummaryResponse> content;
+    private int number;
+    private int size;
     private long totalElements;
     private int totalPages;
     private boolean first;
     private boolean last;
 }
+
